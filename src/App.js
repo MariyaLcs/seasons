16beed7aaaf21a9565aff563129d61267ee6d8c5
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-02 12:36:01 +0100

    handling errors and conditionally rendering content

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super();
-    this.state = { latitude: 40 };
+    this.state = { latitude: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ latitude: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
   render() {
-    return <div>Latitude: {this.state.latitude}</div>;
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.latitude) {
+      return <div>Latitude: {this.state.latitude}</div>;
+    }
+    return <div>Loading!</div>;
   }
 }
 
