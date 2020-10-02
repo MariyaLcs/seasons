33c87ccb3cb97b336235ed0d155a2c7c9ddbdf18
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-02 10:06:51 +0100

    refactor from functional to class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>HOHOHO</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
+  }
+}
 
 export default App;
