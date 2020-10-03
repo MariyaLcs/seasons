9cf1129922291e9461713bc1ea17739a9c4df49e
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-03 15:21:36 +0100

    adding Spinner component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { latitude: null, errorMessage: "" };
@@ -23,7 +24,7 @@ class App extends React.Component {
         </div>
       );
     }
-    return <div>Loading!</div>;
+    return <Spinner />;
   }
 }
 
