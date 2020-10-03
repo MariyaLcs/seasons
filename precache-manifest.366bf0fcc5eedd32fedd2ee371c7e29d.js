2f28849f1d6606eb044706abf2f75703efa250bf
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-03 16:53:06 +0100

    Updates

---
+++ precache-manifest.366bf0fcc5eedd32fedd2ee371c7e29d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d089089fd43e877c4da0aacd86ef9d7f",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "acb2ab604b6eec3ce57c",
+    "url": "/seasons/static/css/main.31f81a7c.chunk.css"
+  },
+  {
+    "revision": "13313a6daf56bcbf5c75",
+    "url": "/seasons/static/js/2.a6e3a673.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.a6e3a673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acb2ab604b6eec3ce57c",
+    "url": "/seasons/static/js/main.01de7d8b.chunk.js"
+  },
+  {
+    "revision": "7aa38b62a80534558f86",
+    "url": "/seasons/static/js/runtime-main.53d2d0be.js"
+  }
+]);
